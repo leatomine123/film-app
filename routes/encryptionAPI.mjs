9ee134b0e77fd4b2e755b080367dc3e048497ab0
lea tomine bøe <leatb@uia.no>
2023-03-21 17:56:13 +0100

    changes

---
+++ routes/encryptionAPI.mjs
@@ -0,0 +1,43 @@
+import express from 'express'
+import apiEndpoints from "../public/js/apiEndpoints.mjs";
+//import { encrypt,decrypt } from '../modules/chipher.mjs';
+import StorageManager from '../modules/storageManager.mjs';
+
+const {API_ENDPOINTS, USER_ENDPOINT}  = apiEndpoints;
+
+
+const connectionString = process.env.DATABASE_URL;
+
+
+const encryptionAPIRoute = express.Router();
+
+encryptionAPIRoute.use(express.json());
+
+encryptionAPIRoute.post(API_ENDPOINTS.encrypt.endpoint, async function(req,res,next){
+    
+    const msg = req.body.msg;
+    const shift = req.body.shift;
+    const chipherText = encrypt(msg,shift);
+    
+    const secretID = await (new StorageManager(connectionString)).saveSecret(chipherText);
+
+    res.json({secretID}).end();
+
+    next();
+})
+
+encryptionAPIRoute.post(`${API_ENDPOINTS.decrypt.endpoint}/:id`, async function(req,res,next){
+    
+    const secretID = req.params.id
+    const shift = req.body.shift;
+
+    const chipherText  = await (new StorageManager(connectionString)).retrieveSecret(secretID);
+    const clearText = decrypt(chipherText,shift);
+
+    res.json({clearText}).end();
+
+    
+    next();
+})
+
+export default encryptionAPIRoute
---
+++ server.mjs
@@ -0,0 +1,29 @@
+import express from 'express'
+//import apiEndpoints from './public/js/apiEndpoints.mjs';
+//import encryptionAPIRoute from './routes/encryptionAPI.mjs';
+//import userAPIRoute from './routes/userAPI.mjs'
+
+//const {API_ENDPOINTS, USER_ENDPOINT}  = apiEndpoints;
+
+const server = express();
+const port = (process.env.PORT || 8080);
+
+console.log("Port set to", port);
+
+server.set('port', port);
+server.use(express.static('public'));
+
+server.use((req,res,next)=>{
+    console.log(req.url);
+    next();
+})
+
+server.use(express.json());
+
+//server.use(API_ENDPOINTS.base,encryptionAPIRoute);
+//server.use("/user",userAPIRoute);
+
+
+server.listen(server.get('port'), function () {
+console.log('server running', server.get('port'));
+});
